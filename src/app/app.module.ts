9524710728ef256b7c171bcbccfd3962434d315c
monu_jangid76 <jangidrohit817@gmail.com>
2023-10-04 15:00:56 +0530

    toolbar add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { EmployeeToolbarComponent } from './shared/component/employee-toolbar/employee-toolbar.component';
@@ -19,6 +22,9 @@ import { EmployeeScreenComponent } from './shared/component/employee-screen/empl
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
