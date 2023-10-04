d82d2d356dda9038bb86dc4055fbf456c8f7df51
monu_jangid76 <jangidrohit817@gmail.com>
2023-10-04 14:53:17 +0530

    big changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { EmployeeToolbarComponent } from './shared/component/employee-toolbar/employee-toolbar.component';
+import { CreateEmployeeDialogComponent } from './shared/models/create-employee-dialog/create-employee-dialog.component';
+import { EmployeeScreenComponent } from './shared/component/employee-screen/employee-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeToolbarComponent,
+    CreateEmployeeDialogComponent,
+    EmployeeScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/component/employee-screen/employee-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-screen',
+  templateUrl: './employee-screen.component.html',
+  styleUrls: ['./employee-screen.component.scss']
+})
+export class EmployeeScreenComponent {
+
+}
