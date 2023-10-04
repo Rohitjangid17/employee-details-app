a4ae152fd7b42a847d7c598c296b98bdc23b58b5
monu_jangid76 <jangidrohit817@gmail.com>
2023-10-04 14:46:14 +0530

    a huge changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
+
+import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,8 +13,6 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule,
-    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
