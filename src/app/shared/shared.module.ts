108d08d4f7b1154f8b578718d2c8c769eeecaa1a
monu_jangid76 <jangidrohit817@gmail.com>
2023-10-04 14:34:07 +0530

    add toolbar component && interface

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddEmployeeComponent } from './component/add-employee/add-employee.component';
 import { EmployeeScreenComponent } from './component/employee-screen/employee-screen.component';
+import { EmployeeToolbarComponent } from './component/employee-toolbar/employee-toolbar.component';
 
 @NgModule({
   declarations: [
     AddEmployeeComponent,
-    EmployeeScreenComponent
+    EmployeeScreenComponent,
+    EmployeeToolbarComponent
   ],
   imports: [
     CommonModule
